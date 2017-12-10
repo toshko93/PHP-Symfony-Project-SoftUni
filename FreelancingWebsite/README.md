fa8df990b4ca6d404fa8ef426f3faf33bbd71504
toshko93 <=>
2017-12-10 14:28:22 +0200

    Project installed, occurences of AppBundle renamed to FreelancingWebsiteBundle

---
+++ FreelancingWebsite/README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 10, 2017, 1:21 pm.
---
+++ FreelancingWebsite/src/FreelancingWebsiteBundle/FreelancingWebsiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FreelancingWebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FreelancingWebsiteBundle extends Bundle
+{
+}
